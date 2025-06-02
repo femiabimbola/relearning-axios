f26f93d3d514619df5596ce3217ca9f3f05bcd32
femi abimbola <femiabimbola6@gmail.com>
2025-06-02 11:44:23 -0700

    updated the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-// import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
+import store from "@/redux/store";
 
 export const metadata: Metadata = {
   title: "Working with Axios",
@@ -14,10 +15,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`flex justify-center items-center h-screen antialiased`}
-      >
-        {children}
+      <body className={`flex justify-center items-center h-screen antialiased`}>
+        <Provider store={store}>{children}</Provider>
       </body>
     </html>
   );
--- learning.md
+++ learning.md
@@ -5,6 +5,8 @@ I created a folder called Redux to host all the context code
 Then, I created the action.ts folder
 > The action folder calls the the api
 
-Then, I proceeded to create reducer.ts, this makes uses of the action
+Then, I proceeded to create reducer.ts, this makes uses of the action, each actions can have its own reducers,
 
-Then I create store
+Then I create store using the reducers
+
+Then I put the store in the root of the application
