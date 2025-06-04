df70a994d1c7d7d84e87f4adb1ef7db1c8f20482
femi abimbola <femiabimbola6@gmail.com>
2025-06-04 14:18:49 -0700

    working on the create user

--- components/CreateUser.tsx
+++ components/CreateUser.tsx
@@ -1,5 +1,106 @@
-export const CreateUser = () =>{
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Name must be at least 2 characters.",
+  }),
+  email: z.string().email({
+    message: "Please enter a valid email address.",
+  }),
+  gender: z.string().email({
+    message: "Please enter a valid email address.",
+  }),
+  salary: z.number().min(0, {
+    message: "Salary must be a non-negative number.",
+  }),
+})
+
+export const CreateUser = () => {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "", email:"", gender: "", salary:0
+    },
+  })
+
+   const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
+
   return (
-    <div>Create User</div>
+    <Form {...form} >
+    <h1 className="text-center text-2xl font-bold"> Register a new user</h1>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 w-1/3 mx-auto">
+      <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Name</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter your name" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="email"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Email</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter your email" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="gender"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Gender</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter your gender" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="salary"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Salary</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter your Salary" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <Button type="submit" className="text-center">Submit</Button>
+    </form>
+  </Form>
   )
 }
