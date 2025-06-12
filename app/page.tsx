8f437fcc456acc14763c4fad69033363a3d065e1
femi abimbola <femiabimbola6@gmail.com>
2025-06-11 23:48:22 -0700

    final one

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 import { List } from "@/components/List";
 import ReduxProvider from "./providers";
 import { CreateUser } from "@/components/CreateUser";
-import { CreateUser2 } from "@/components/User";
 
 const  Home = () =>{
   return (
     <ReduxProvider>
-      {/* <CreateUser /> */}
       <CreateUser/>
-       <List />
+      <List />
     </ReduxProvider>
      
   )   
